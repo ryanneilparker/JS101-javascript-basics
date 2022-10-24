693b6fbf36501cde0eff142f696dfc89296f014a
ryanneilparker <ryanneilparker@gmail.com>
2022-10-24 20:16:03 +0000

    Add odd.js

---
+++ easy_1/odd.js
@@ -0,0 +1,16 @@
+/* Odd Numbers
+Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+PROBLEM
+inputs: none
+ouputs: print all nums from 1..99
+req: inclusive
+*/
+
+function printNums() {
+  for(let i = 1; i <= 99; i++) {
+    console.log(i);
+  }
+}
+
+printNums();
