20683ca8566da04d6fef442ddef8f6a7ef48b469
ryanneilparker <ryanneilparker@gmail.com>
2022-10-25 11:49:02 +0000

    Add square.js

---
+++ easy_2/square.js
@@ -0,0 +1,14 @@
+/*
+Squaring an Argument
+Using the multiply() function from the "Multiplying Two Numbers" problem, 
+write a function that computes the square of its argument (the square is the result of multiplying a number by itself).
+*/
+
+const multiply = (num1, num2) => num1 * num2;
+
+function square(num) {
+  return multiply(num, num);
+}
+
+console.log(square(5) === 25); // logs true
+console.log(square(-8) === 64); // logs true
